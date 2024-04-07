0e493b179ea36c5900189e81ed70d69cfc1da190
Mikolaj <mikolaj.m.zulewski@gmail.com>
2024-04-07 11:00:22 +0200

    klasy point i main

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        point p = new point(3.5, 4.2);
+
+        
+        System.out.println("Współrzędne punktu:");
+        System.out.println("x: " + p.getX());
+        System.out.println("y: " + p.getY());
+    }
+}
